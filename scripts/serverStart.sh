72fec71a9fce54f8c1302f9f96b37f9a6a799697
root <paikchris@gmail.com>
2017-11-16 09:44:11 +0000

    Update

--- scripts/serverStart.sh
+++ scripts/serverStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SHELL=/bin/bash
-PATH=/root/.sdkman/candidates/grails/current/bin:/root/.sdkman/candidates/groovy/current/bin:/root/.sdkman/candidates/grails/current/bin:/usr/local/rvm/gems/ruby-1.9.2-p330/bin:/usr/local/rvm/gems/ruby-1.9.2-p330@global/bin:/usr/local/rvm/rubies/ruby-1.9.2-p330/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/rvm/bin:/root/bin
+PATH=$PATH:/root/.sdkman/candidates/grails/current/bin:/root/.sdkman/candidates/groovy/current/bin:/root/.sdkman/candidates/grails/current/bin:/root/bin
+export GRAILS_OPTS="-server -Xmx768M -Xms64M -XX:PermSize=32m -XX:MaxPermSize=256m -Dfile.encoding=UTF-8 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/universe/Neeis/logs -XX:ErrorFile=/universe/Neeis/logs/error.log"
 
 cd /universe/Neeis
-export GRAILS_OPTS="-XX:MaxPermSize=2000m -Xms256m  -Xmx4000M -server -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/universe/Neeis/logs -XX:ErrorFile=/universe/Neeis/logs/error.log "
 ( nohup grails -noreloading run-app -https  --non-interactive 2>&1 > /dev/null & )
