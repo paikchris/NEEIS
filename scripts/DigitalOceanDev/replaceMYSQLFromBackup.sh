4de5f37f93c1d7ea81da77ebce8945825a32384a
Unknown <paikchris@gmail.com>
2018-02-28 14:05:38 -0800

    db

--- scripts/DigitalOceanDev/replaceMYSQLFromBackup.sh
+++ scripts/DigitalOceanDev/replaceMYSQLFromBackup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-chmod 755 /home/vagrant/backup_mysql.sql
+chmod 755 /universe/AdminScripts/backup_mysql.sql
 MYSQLPW='Perseverence!2'
 
 mysql -u root -p$MYSQLPW -e "DROP DATABASE IF EXISTS neeisPortal;"
- 
+
 mysqladmin -u root -p$MYSQLPW create neeisPortal
-mysql -u root -p$MYSQLPW neeisPortal --connect-expired-password  < /home/vagrant/backup_mysql.sql
+mysql -u root -p$MYSQLPW neeisPortal --connect-expired-password  < /universe/AdminScripts/backup_mysql.sql
